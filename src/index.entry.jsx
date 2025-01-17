68fdcb07a130eb0c714299e3987b16688d1199fe
Stefan Stefanov <s.h.stefanov@mail.bg>
2025-01-17 22:22:34 +0000

    WIP

--- src/index.entry.jsx
+++ src/index.entry.jsx
@@ -8,3 +8,11 @@ const rootElement = document.querySelector("#root");
 const rootNode = createRoot(rootElement);
 
 rootNode.render(<App />);
+
+
+
+
+
+
+// https://github.com/shstefanov/orbits-engine-v2-examples/blob/development/src/examples/objects/MeshLoaderExample.jsx
+console.error("TODO: fix links to Github source");
